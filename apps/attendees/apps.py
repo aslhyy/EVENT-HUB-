d8609ebf9c12fb4c2fcfe4e14c8f12ba0523b2de
aslhyy <aslhynicol0936@gmail.com>
2025-12-11 10:42:21 -0500

    Delete apps/attendees directory

--- apps/attendees/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class AttendeesConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'apps.attendees'
-    verbose_name = 'Gestión de Asistentes'
