1cc2d3c27701351d0ed9efa2417eef7bafd3f1f0
Sarah Castro <sarahsofiacastro338@gmail.com>
2025-12-11 19:19:37 -0500

    App Events

--- apps/sponsors/events/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class EventsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'apps.events'
-    verbose_name = 'Gestión de Eventos'c:\Users\GO\Downloads\nuevos archivos\events\filters.py
